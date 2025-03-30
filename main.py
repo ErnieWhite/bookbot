4e3725a3911adcac59eb28653187557775ff08a1
ErnieWhite <ernie.white.68@gmail.com>
2025-03-30 10:03:58 -0500

    updated display format

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from stats import get_num_words
 from stats import get_character_counts
+from stats import get_sorted_character_counts 
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    filepath = './books/frankenstein.txt'
+    print("============ BOOKBOT ============")
+
+    filepath = 'books/frankenstein.txt'
+    print(f"Analyzing book found at {filepath}...")
+
     contents = get_book_text(filepath)
     num_words = get_num_words(contents)
-    print(f"{num_words} words found in the document")
+
+
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
     character_counts = get_character_counts(contents)
-    for c in character_counts:
-        print(f"'{c}': {character_counts[c]}")
+    sorted_character_counts = get_sorted_character_counts(character_counts)
+
+    print("--------- Character Count -------")
+    for count in sorted_character_counts:
+        c = next(iter(count))
+        if c.isalpha():
+            print(f"{next(iter(count))}: {count[next(iter(count))]}")
 
 main()

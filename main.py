81fb53d9238cf289a0a4127fc51668823f1e83a5
ErnieWhite <ernie.white.68@gmail.com>
2025-03-30 10:18:24 -0500

    add command line support

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from stats import get_num_words
 from stats import get_character_counts
 from stats import get_sorted_character_counts 
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
+    # check to make sure they specified the book to process
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
     print("============ BOOKBOT ============")
 
-    filepath = 'books/frankenstein.txt'
     print(f"Analyzing book found at {filepath}...")
 
     contents = get_book_text(filepath)

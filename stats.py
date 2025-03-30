86b37210571b7fa4089ca7577a6fbe1e3839f921
ErnieWhite <ernie.white.68@gmail.com>
2025-03-30 09:05:21 -0500

    added get_character_counts

--- stats.py
+++ stats.py
@@ -1,4 +1,10 @@
+def get_character_counts(contents):
+    counts = {}
+    for c in contents.lower():
+        counts[c] = counts.get(c, 0) + 1
+    return counts
+
+
 def get_num_words(contents):
     word_count = len(contents.split())
     return word_count
-

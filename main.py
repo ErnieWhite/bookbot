eeef8a10d17478215708f7e840c5fe70bf367138
ErnieWhite <ernie.white.68@gmail.com>
2025-03-30 09:05:52 -0500

    modified to display character counts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_character_counts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,5 +10,8 @@ def main():
     contents = get_book_text(filepath)
     num_words = get_num_words(contents)
     print(f"{num_words} words found in the document")
+    character_counts = get_character_counts(contents)
+    for c in character_counts:
+        print(f"'{c}': {character_counts[c]}")
 
 main()
